8748bb594747b4ce721bfefdaec4249ef4787378
wparke4 <willparke4@gmail.com>
2023-10-15 11:56:18 -0500

    Wordrift is parent component. Grid and Tilerack separated

--- components/wordriftFolder/Wordrift.vue
+++ components/wordriftFolder/Wordrift.vue
@@ -1,107 +1,16 @@
 <template>
-    <div class="game-board">
-        <div
-            v-for="row in grid"
-            :key="row.id"
-            class="grid-row"
-        >
-            <div
-                v-for="cell in row.cells"
-                :key="cell.id"
-                class="grid-cell"
-                @dragover.prevent
-                @drop="event => handleDrop(event, cell)"
-                @click="cellClickHandler(cell)"
-            >
-                {{  cell.letter }}
-            </div>
-        </div>
-    </div>
+    <Grid />
     <TileRack />
 </template>
 
 <script>
 import TileRack from "@/components/wordriftFolder/TileRack.vue";
+import Grid from "@/components/wordriftFolder/Grid.vue";
 
 export default {
-    data() {
-        return {
-            grid: this.createGrid(10,20)
-        }
-    },
     components: {
-        TileRack
+        TileRack,
+        Grid
     },
-    methods: {
-        createGrid(rows, cols) {
-            //Create a psuedo array of rows
-            return Array.from({ length: rows }, (_, rowIndex) => ({
-                id: rowIndex,
-                /* Create an array of length cols for each row
-                Map over each element in the array and create and object
-                with an id of row index and col index as well as letter parameter */
-                cells: Array.from({ length: cols }, (_, colIndex) => ({
-                    id: `${rowIndex}-${colIndex}`,
-                    letter: null
-                }))
-            }))
-        },
-        cellClickHandler(cell) {
-            console.log('Cell clicked: ', cell);
-            /*
-            To Do: implement logic for clicking cell
-            I would only want to do something if the cell has a letter.
-            The action would be to pop that letter back into player's hand
-            if the word has not been submitted and validated yet.
-            */
-        },
-        handleDrop(event, cell) {
-            // retrieve tile data from the event
-            const tileData = JSON.parse(event.dataTransfer.getData("tile"));
-
-            // logic to place tile on the grid
-            this.placeTileOnGrid(tileData, cell);
-        },
-        placeTileOnGrid(tileData, cell) {
-            // find the row and col index of the cell
-            const [rowIndex, colIndex] = cell.id.split("-");
-
-
-
-            if (!this.grid[rowIndex].cells[colIndex].letter) {
-                this.grid[rowIndex].cells[colIndex].letter = tileData.letter;
-
-                // Communicate back to tile rack to remove this tile from there
-                this.$emit('tile-placed', tileData.id);
-            } else {
-                console.log('Cell is already occupied');
-            }
-        }
-    }
 }
 </script>
-
-<style scoped>
-.game-board {
-  display: flex;
-  flex-direction: column;
-}
-
-.grid-row {
-  display: flex;
-}
-
-.grid-cell {
-  height: 40px;
-  width: 40px;
-  border: 1px solid #ddd;
-  text-align: center;
-  line-height: 40px;
-  cursor: pointer;
-}
-
-.grid-cell:hover {
-  background-color: #eee;
-}
-</style>
-
