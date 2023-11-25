57ef8f2034e9c9f3afd79300088670c47195f3e3
wparke4 <willparke4@gmail.com>
2023-11-25 10:39:49 -0500

    Removed TurnManager file

--- components/grogGamesFolder/GrogGamesDb.vue
+++ components/grogGamesFolder/GrogGamesDb.vue
@@ -1,22 +1,12 @@
 <template>
     <PlayerDisplay />
     <GameManager />
-    <TurnManager />
 </template>
 
-
-
-
 <script setup>
+
 import GameManager from "@/components/grogGamesFolder/GameManager.vue";
 import PlayerDisplay from "@/components/grogGamesFolder/PlayerDisplay.vue";
-import TurnManager from "@/components/grogGamesFolder/TurnManager.vue";
-
-const gameStarted = useState("gameStarted", () => false);
-gameStarted.value = false;
-
-
-//const gameStarted = useState("gameStarted", () => false);
 
 </script>
 <style scoped>
