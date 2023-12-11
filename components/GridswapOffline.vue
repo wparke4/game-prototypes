360ef5fff0d8f1ed7a6c08e54a45e54bbc34cb6d
wparke4 <willparke4@gmail.com>
2023-12-10 17:00:10 -0800

    New row and col calculated based on input

--- components/GridswapOffline.vue
+++ components/GridswapOffline.vue
@@ -1,10 +1,13 @@
 <template>
     <div>
-        <div v-for="(row, rowIndex) in grid" class="flex co">
+        <div
+            v-for="(row, rowIndex) in grid"
+            class="flex col"
+        >
             <div
                 v-for="(cell, colIndex) in row"
                 class="flex w-20 h-20 border-gray-500 border rounded-xs"
-                :class="{ 'bg-black': cell.isHazard }"
+                :class="{ 'bg-black': cell.isHazard, 'bg-green-400': cell.isOccupied}"
                 :key="'cell-' + rowIndex + '-' + colIndex"
             >
                 {{ rowIndex }}, {{ colIndex }}
@@ -21,7 +24,6 @@ const rows = 5;
 const cols = 5;
 
 const createGrid = () => {
-    console.log("create grid called")
     for (let i = 0; i < rows; i++) {
         let currRow = []
         //create array of cols for this row
@@ -30,17 +32,65 @@ const createGrid = () => {
         }
         grid.value.push(currRow)
     }
-    console.log(grid)
 }
 
 const createRandomHazards = () => {
     grid.value[0][0].isHazard = true
-    console.log("create rand..", grid.value[0][0])
 }
 
-console.log("what the fuck")
+const createPlayer = () => {
+    grid.value[0][1].isOccupied = true
+}
+
+const handleKeydown = (event) => {
+  switch(event.key) {
+    case 'ArrowUp':
+      // handle up arrow key
+      handleMove(-1,0)
+      break;
+    case 'ArrowDown':
+      // handle down arrow key
+      handleMove(1, 0)
+      break;
+    case 'ArrowLeft':
+      // handle left arrow key
+      handleMove(0, -1)
+      break;
+    case 'ArrowRight':
+      // handle right arrow key
+      handleMove(0, 1)
+      break;
+  }
+}
+
+const handleMove = (rowDelta, colDelta) => {
+    const startingRow = occupiedCell.value.row
+    const startingCol = occupiedCell.value.col
+
+    const newRow = startingRow + rowDelta
+    const newCol = startingCol + colDelta
+
+    console.log("occupiedCell ", occupiedCell)
+    console.log("newRow, newCol ", newRow, newCol)
+}
+
+const occupiedCell = computed(() => {
+    for (let i = 0; i < grid.value.length; i++) {
+        const occupied = grid.value[i].find(element => element.isOccupied)
+        console.log("occupied is ", occupied)
+        if (occupied) { return occupied}
+    }
+})
+
+
 onMounted(() => {
-   createGrid(),
+   window.addEventListener('keydown', handleKeydown)
+   createGrid()
    createRandomHazards()
-});
+   createPlayer()
+})
+
+onUnmounted(() => {
+  window.removeEventListener('keydown', handleKeydown)
+})
 </script>
